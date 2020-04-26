bec2378740dac4ffe929bc700585e9d812a056e8
Christa Gammage <crgammage@gmail.com>
2020-04-26 15:36:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,10 +8,8 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  reversed_sentence = []
   sentence.split(" ").collect do |word|
     reversed_word = word.reverse
-    reversed_sentence << reversed_word
   end
   reversed_sentence.join(" ")
 end
